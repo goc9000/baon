8258454c02afc85dd8d72bf755bf0297daf60764
Cristian Dinu <goc9000@gmail.com>
2012-07-10 02:03:24 +0300

    Parse more command-line parameters

--- src/baon.py
+++ src/baon.py
@@ -1,15 +1,17 @@
-import sys
+import sys, argparse
 
 from gui.Gui import Gui
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Mass file renamer.', argument_default=argparse.SUPPRESS)
+    parser.add_argument('base_path', metavar='<base path>', nargs='?', help='A path to the files that are to be renamed')
+    parser.add_argument('--rules', metavar='<rule text>', help='Rules to apply (separate using semicolons)')
+    parser.add_argument('-r', '--scan-recursive', action='store_true', help='Scan subfolders recursively')
+    parser.add_argument('-x', '--use-ext', action='store_true', help='Include extension in renaming process')
+    parser.add_argument('-p', '--use-path', action='store_true', help='Include full path in renaming process')
+    setup = vars(parser.parse_args(sys.argv[1:]))
+    
     gui = Gui()
-    
-    setup = dict()
-    
-    if len(sys.argv) > 1:
-        setup['base_path'] = sys.argv[1]
-    
     if len(setup) > 0:
         gui.setupMainWindow(setup)
     
