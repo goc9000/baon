9a6cb9c7f34a07f618e0109d186b3e2b758687f9
Cristian Dinu <goc9000@gmail.com>
2014-09-24 02:05:26 +0100

    Bugfixed RegexMatch

--- src/baon/logic/ast/matches/pattern/RegexMatch.py
+++ src/baon/logic/ast/matches/pattern/RegexMatch.py
@@ -19,21 +19,27 @@ class RegexMatch(ElementaryPatternMatch):
     pattern = ast_node_field()
     flags = ast_node_field(hide_for_value=set())
 
-    def __init__(self, pattern, flags):
+    def __init__(self, pattern, flags=None):
         ElementaryPatternMatch.__init__(self)
 
         self.pattern = pattern
-        self.flags = flags
+        self.flags = flags if flags is not None else ()
 
     def _get_pattern_impl(self):
-        return "({0})".format(self.pattern)
+        # First compile it without the parantheses, as otherwise we would accept malformed expressions such as /)(/
+        try:
+            re.compile(self.pattern)
+        except re.error:
+            raise RuleCheckException('Error in regular expression')
+
+        return u'({0})'.format(self.pattern)
 
     def _get_flags_impl(self):
-        flags_enum = 0
+        flags_enum = re.U
         for flag in self.flags:
             if flag == 'i':
                 flags_enum |= re.I
             else:
-                raise RuleCheckException("Invalid regex flag '{0}'".format(flag))
+                raise RuleCheckException(u"Invalid regex flag '{0}'".format(flag))
 
         return flags_enum
