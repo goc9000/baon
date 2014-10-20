fd91029a9d5a8c8d7b77e40049da23c94d55c6c6
Cristian Dinu <goc9000@gmail.com>
2014-10-20 01:00:44 +0100

    Added unit test for symlink support in FileScanner

--- src/baon/core/files/__tests__/test_FileScanner.py
+++ src/baon/core/files/__tests__/test_FileScanner.py
@@ -19,14 +19,18 @@ import shutil
 
 class TestFileScanner(TestCase):
     _test_dir_path = None
+    _links_supported = None
 
-    # TODO: Scan for files that: -cannot be renamed (no write access) - cannot be opened (dirs)
-    # TODO: Scan for unicode files
+    # TODO: Scan for files that: -cannot be renamed (no write access) - cannot be opened (dirs) - dangling symlinks
+    # TODO: Scan for non-existent directory
+    # TODO: Scan for unicode files (check out os.path.supports_unicode_filenames)
     # TODO: Do not follow symlinks
+    # TODO: Test that it reports progress
 
     @classmethod
     def setUpClass(cls):
         cls._test_dir_path = tempfile.mkdtemp()
+        cls._links_supported = cls._check_links_supported()
 
         cls._make_file('basic/dir1/file11.txt')
         cls._make_file('basic/dir1/file12')
@@ -44,6 +48,11 @@ class TestFileScanner(TestCase):
         cls._make_dir('empties/dir1/empty1')
         cls._make_file('empties/dir1/file1')
 
+        if cls._links_supported:
+            cls._make_link('links/link1', 'basic/file2.bin')
+            cls._make_link('links/link2', 'basic/dir1')
+            cls._make_link('links/link3', 'dangling')
+
     @classmethod
     def _make_file(cls, file_path):
         dir_name, _ = os.path.split(file_path)
@@ -59,6 +68,29 @@ class TestFileScanner(TestCase):
 
         if not os.path.isdir(full_dir_path):
             os.makedirs(full_dir_path)
+
+    @classmethod
+    def _check_links_supported(cls):
+        full_link_path = os.path.join(cls._test_dir_path, 'temp_link')
+        full_target_path = os.path.join(cls._test_dir_path, '')
+
+        try:
+            os.symlink(full_target_path, full_link_path)
+        except OSError:
+            return False
+
+        os.unlink(full_link_path)
+
+        return True
+
+    @classmethod
+    def _make_link(cls, link_path, target_path):
+        dir_name, _ = os.path.split(link_path)
+        cls._make_dir(dir_name)
+
+        full_link_path = os.path.join(cls._test_dir_path, link_path)
+        full_target_path = os.path.join(cls._test_dir_path, target_path)
+        os.symlink(full_target_path, full_link_path)
 
     @classmethod
     def tearDownClass(cls):
@@ -112,6 +144,34 @@ class TestFileScanner(TestCase):
             recursive=True,
             expected_result=(
                 ('FILE', u'dir1/file1'),
+            )
+        )
+
+    def test_links_non_recursive(self):
+        if not self._links_supported:
+            self.skipTest('Skipping test_links_non_recursive: Symlinks are not supported on this platform')
+
+        self._test_file_scanner(
+            base_path=u'links',
+            recursive=False,
+            expected_result=(
+                ('LINK:DIR', u'link2'),
+                ('LINK:FILE', u'link1'),
+                ('LINK:FILE', u'link3'),
+            )
+        )
+
+    def test_links_recursive(self):
+        if not self._links_supported:
+            self.skipTest('Skipping test_links_recursive: Symlinks are not supported on this platform')
+
+        self._test_file_scanner(
+            base_path=u'links',
+            recursive=True,
+            expected_result=(
+                ('LINK:DIR', u'link2'),  # Symlinks are not followed even in recursive mode
+                ('LINK:FILE', u'link1'),
+                ('LINK:FILE', u'link3'),
             )
         )
 
