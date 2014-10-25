0979efcc10c01aa9fd2f22de5886d786d05cf97e
Cristian Dinu <goc9000@gmail.com>
2014-10-26 00:53:47 +0100

    Factored out general file system test code

---
+++ src/baon/core/__tests__/FileSystemTestCase.py
@@ -0,0 +1,72 @@
+# baon/core/__tests__/FileSystemTestCase.py
+#
+# (C) Copyright 2012-present  Cristian Dinu <goc9000@gmail.com>
+# 
+# This file is part of BAON.
+#
+# Licensed under the GPL-3
+
+
+from unittest import TestCase
+
+import os
+import tempfile
+import shutil
+
+
+class FileSystemTestCase(TestCase):
+    _test_dir_path = None
+    _links_supported = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls._test_dir_path = tempfile.mkdtemp()
+        cls._links_supported = cls._check_links_supported()
+        cls.setup_test_files()
+
+    @classmethod
+    def setup_test_files(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls._test_dir_path)
+
+    @classmethod
+    def _make_file(cls, file_path):
+        dir_name, _ = os.path.split(file_path)
+        cls._make_dir(dir_name)
+
+        full_file_path = os.path.join(cls._test_dir_path, file_path)
+        with file(full_file_path, 'w') as _:
+            pass
+
+    @classmethod
+    def _make_dir(cls, dir_path):
+        full_dir_path = os.path.join(cls._test_dir_path, dir_path)
+
+        if not os.path.isdir(full_dir_path):
+            os.makedirs(full_dir_path)
+
+    @classmethod
+    def _check_links_supported(cls):
+        full_link_path = os.path.join(cls._test_dir_path, 'temp_link')
+        full_target_path = os.path.join(cls._test_dir_path, '')
+
+        try:
+            os.symlink(full_target_path, full_link_path)
+        except OSError:
+            return False
+
+        os.unlink(full_link_path)
+
+        return True
+
+    @classmethod
+    def _make_link(cls, link_path, target_path):
+        dir_name, _ = os.path.split(link_path)
+        cls._make_dir(dir_name)
+
+        full_link_path = os.path.join(cls._test_dir_path, link_path)
+        full_target_path = os.path.join(cls._test_dir_path, target_path)
+        os.symlink(full_target_path, full_link_path)
--- src/baon/core/files/__tests__/test_FileScanner.py
+++ src/baon/core/files/__tests__/test_FileScanner.py
@@ -7,20 +7,13 @@
 # Licensed under the GPL-3
 
 
-from unittest import TestCase
-
+from baon.core.__tests__.FileSystemTestCase import FileSystemTestCase
 from baon.core.files.FileScanner import FileScanner
 
-
 import os
-import tempfile
-import shutil
 
 
-class TestFileScanner(TestCase):
-    _test_dir_path = None
-    _links_supported = None
-
+class TestFileScanner(FileSystemTestCase):
     # TODO: Scan for files that: -cannot be renamed (no write access) - cannot be opened (dirs) - dangling symlinks
     # TODO: Scan for non-existent directory
     # TODO: Scan for unicode files (check out os.path.supports_unicode_filenames)
@@ -28,10 +21,7 @@ class TestFileScanner(TestCase):
     # TODO: Test that it reports progress
 
     @classmethod
-    def setUpClass(cls):
-        cls._test_dir_path = tempfile.mkdtemp()
-        cls._links_supported = cls._check_links_supported()
-
+    def setup_test_files(cls):
         cls._make_file('basic/dir1/file11.txt')
         cls._make_file('basic/dir1/file12')
         cls._make_file('basic/dir2/dir21/file211')
@@ -52,49 +42,6 @@ class TestFileScanner(TestCase):
             cls._make_link('links/link1', 'basic/file2.bin')
             cls._make_link('links/link2', 'basic/dir1')
             cls._make_link('links/link3', 'dangling')
-
-    @classmethod
-    def _make_file(cls, file_path):
-        dir_name, _ = os.path.split(file_path)
-        cls._make_dir(dir_name)
-
-        full_file_path = os.path.join(cls._test_dir_path, file_path)
-        with file(full_file_path, 'w') as _:
-            pass
-
-    @classmethod
-    def _make_dir(cls, dir_path):
-        full_dir_path = os.path.join(cls._test_dir_path, dir_path)
-
-        if not os.path.isdir(full_dir_path):
-            os.makedirs(full_dir_path)
-
-    @classmethod
-    def _check_links_supported(cls):
-        full_link_path = os.path.join(cls._test_dir_path, 'temp_link')
-        full_target_path = os.path.join(cls._test_dir_path, '')
-
-        try:
-            os.symlink(full_target_path, full_link_path)
-        except OSError:
-            return False
-
-        os.unlink(full_link_path)
-
-        return True
-
-    @classmethod
-    def _make_link(cls, link_path, target_path):
-        dir_name, _ = os.path.split(link_path)
-        cls._make_dir(dir_name)
-
-        full_link_path = os.path.join(cls._test_dir_path, link_path)
-        full_target_path = os.path.join(cls._test_dir_path, target_path)
-        os.symlink(full_target_path, full_link_path)
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls._test_dir_path)
 
     def test_basic_non_recursive(self):
         self._test_file_scanner(
