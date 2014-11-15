ff45cbf6e026428d03fac46bfa699e1d15483955
Cristian Dinu <goc9000@gmail.com>
2014-11-15 23:18:44 +0000

    Bugfix for decode_string_literal()
    Unicode escapes would not be decoded if they were the only content in the string

--- src/baon/core/utils/baon_utils.py
+++ src/baon/core/utils/baon_utils.py
@@ -43,10 +43,10 @@ def decode_baon_string_literal(literal):
         
         char_after_escape = literal[next_escape_pos+1]
         pos = next_escape_pos + 2
-        
+
         if char_after_escape in SIMPLE_ESCAPES:  # simple escape
             output_parts.append(SIMPLE_ESCAPES[char_after_escape])
-        elif char_after_escape == 'u' and pos < len(literal)-4 \
+        elif char_after_escape == 'u' and pos <= len(literal)-4 \
                 and all(literal[pos+i] in string.hexdigits for i in xrange(4)):  # Unicode escape
             output_parts.append(unichr(int(literal[pos:pos+4], 16)))
             pos += 4
@@ -62,7 +62,7 @@ def decode_baon_string_literal(literal):
         else:  # unsupported escape
             output_parts.append(u'\\')
             output_parts.append(char_after_escape)
-    
+
     output_parts.append(literal[pos:])
-    
+
     return u''.join(output_parts)
