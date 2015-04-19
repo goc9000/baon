c3a7fb13fa87ee63dcb990df7e7754f9a2651ef3
Cristian Dinu <goc9000@gmail.com>
2015-04-19 03:55:13 +0100

    Store inputs in BAON QT core

--- src/baon/ui/qt_gui/BAONQtCore.py
+++ src/baon/ui/qt_gui/BAONQtCore.py
@@ -14,8 +14,29 @@ class BAONQtCore(QObject):
     prologue_finished = pyqtSignal()
     has_shutdown = pyqtSignal()
 
+    # Inputs
+    _base_path = ''
+    _scan_recursive = False
+    _rules_text = ''
+    _use_extension = False
+    _use_path = False
+
     def __init__(self, args):
         super().__init__()
+
+        self._init_inputs(args)
+
+    def _init_inputs(self, args):
+        if args.base_path is not None:
+            self._base_path = args.base_path
+        if args.scan_recursive is not None:
+            self._scan_recursive = args.scan_recursive
+        if args.rules_text is not None:
+            self._rules_text = args.rules_text
+        if args.use_extension is not None:
+            self._use_extension = args.use_extension
+        if args.use_path is not None:
+            self._use_path = args.use_path
 
     @pyqtSlot()
     def start(self):
