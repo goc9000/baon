83775e501c815f1ee3944a5d87932bb635a4fa53
Cristian Dinu <goc9000@gmail.com>
2015-05-12 01:07:50 +0100

    Add rename mechanism in BAON QT Core

--- src/baon/ui/qt_gui/BAONQtCore.py
+++ src/baon/ui/qt_gui/BAONQtCore.py
@@ -19,6 +19,7 @@ from baon.core.utils.progress.ProgressInfo import ProgressInfo
 
 from baon.core.files.scan_files import scan_files
 from baon.core.parsing.parse_rules import parse_rules
+from baon.core.renaming.rename_files import rename_files
 
 
 class BAONQtCore(CancellableWorkerMixin, QObject):
@@ -26,6 +27,7 @@ class BAONQtCore(CancellableWorkerMixin, QObject):
         NOT_STARTED = 'not_started'
         READY = 'ready'
         SCANNING_FILES = 'scanning_files'
+        RENAMING_FILES = 'renaming_files'
         SHUTDOWN = 'shutdown'
 
     prologue_finished = pyqtSignal()
@@ -40,6 +42,15 @@ class BAONQtCore(CancellableWorkerMixin, QObject):
 
     rules_ok = pyqtSignal()
     rules_error = pyqtSignal(BAONError)
+
+    not_ready_to_rename = pyqtSignal()
+    no_files_to_rename = pyqtSignal()
+
+    started_renaming_files = pyqtSignal()
+    rename_files_progress = pyqtSignal(ProgressInfo)
+    rename_files_ok = pyqtSignal()
+    rename_files_error = pyqtSignal(BAONError)
+    renamed_files_updated = pyqtSignal(list)
 
     ready = pyqtSignal()
 
@@ -76,24 +87,38 @@ class BAONQtCore(CancellableWorkerMixin, QObject):
 
     @pyqtSlot(str)
     def update_base_path(self, base_path):
-        assert self._state in [self.State.READY, self.State.SCANNING_FILES]
+        assert self._state in [self.State.READY, self.State.SCANNING_FILES, self.State.RENAMING_FILES]
 
         self._base_path = base_path
         self._on_scan_files_inputs_changed()
 
     @pyqtSlot(bool)
     def update_scan_recursive(self, scan_recursive):
-        assert self._state in [self.State.READY, self.State.SCANNING_FILES]
+        assert self._state in [self.State.READY, self.State.SCANNING_FILES, self.State.RENAMING_FILES]
 
         self._scan_recursive = scan_recursive
         self._on_scan_files_inputs_changed()
 
     @pyqtSlot(str)
     def update_rules_text(self, rules_text):
-        assert self._state in [self.State.READY, self.State.SCANNING_FILES]
+        assert self._state in [self.State.READY, self.State.SCANNING_FILES, self.State.RENAMING_FILES]
 
         self._rules_text = rules_text
         self._recompile_rules()
+
+    @pyqtSlot(bool)
+    def update_use_path(self, use_path):
+        assert self._state in [self.State.READY, self.State.SCANNING_FILES, self.State.RENAMING_FILES]
+
+        self._use_path = use_path
+        self._on_rename_files_inputs_changed()
+
+    @pyqtSlot(bool)
+    def update_use_extension(self, use_extension):
+        assert self._state in [self.State.READY, self.State.SCANNING_FILES, self.State.RENAMING_FILES]
+
+        self._use_extension = use_extension
+        self._on_rename_files_inputs_changed()
 
     @pyqtSlot()
     def shutdown(self):
@@ -134,6 +159,8 @@ class BAONQtCore(CancellableWorkerMixin, QObject):
             self.has_shutdown.emit()
         elif self._state == self.State.SCANNING_FILES:
             self.started_scanning_files.emit()
+        elif self._state == self.State.RENAMING_FILES:
+            self.started_renaming_files.emit()
         elif self._state == self.State.READY:
             self.ready.emit()
 
@@ -191,4 +218,49 @@ class BAONQtCore(CancellableWorkerMixin, QObject):
             self._on_rename_files_inputs_changed()
 
     def _on_rename_files_inputs_changed(self):
-        pass
+        if self._state == self.State.SCANNING_FILES:
+            return
+
+        self._rename_files()
+
+    def _rename_files(self):
+        if self._scanned_files is None or self._rules is None:
+            self._on_rename_files_finished(None)
+            return
+        if len(self._scanned_files) == 0:
+            self._on_rename_files_finished([])
+            return
+
+        self._switch_state(self.State.RENAMING_FILES)
+        self._start_worker(
+            work=lambda check_abort: rename_files(
+                self._scanned_files,
+                self._rules,
+                use_path=self._use_path,
+                use_extension=self._use_extension,
+                on_progress=lambda progress: self.rename_files_progress.emit(progress),
+                check_abort=check_abort,
+            ),
+            on_finished=self._on_rename_files_finished,
+        )
+
+    def _on_rename_files_finished(self, result):
+        self._switch_state(self.State.READY)
+        self._update_renamed_files(result)
+
+    def _update_renamed_files(self, result):
+        if result is not None and not isinstance(result, BAONError) and len(result) > 0:
+            self._renamed_files = result
+        else:
+            self._renamed_files = None
+
+        self.renamed_files_updated.emit(self._renamed_files if self._renamed_files is not None else [])
+
+        if result is None:
+            self.not_ready_to_rename.emit()
+        elif isinstance(result, BAONError):
+            self.rename_files_error.emit(result)
+        elif len(result) == 0:
+            self.no_files_to_rename.emit()
+        else:
+            self.rename_files_ok.emit()
