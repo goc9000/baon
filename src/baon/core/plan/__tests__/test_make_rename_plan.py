1014a48963ce97343adc59d02718fe363c760ea2
Cristian Dinu <goc9000@gmail.com>
2015-10-25 13:03:26 +0000

    Split normal and error tests for make_rename_plan

--- src/baon/core/plan/__tests__/test_make_rename_plan.py
+++ src/baon/core/plan/__tests__/test_make_rename_plan.py
@@ -7,21 +7,10 @@
 # Licensed under the GPL-3
 
 
-import os
-
-from baon.core.__tests__.FileSystemTestCase import FileSystemTestCase
-from baon.core.parsing.parse_rules import parse_rules
-from baon.core.files.scan_files import scan_files
-from baon.core.renaming.rename_files import rename_files
-
-from baon.core.plan.__errors__.make_rename_plan_errors import MakeRenamePlanError
-
-from baon.core.plan.make_rename_plan import make_rename_plan
-
-from baon.core.utils.lang_utils import is_arrayish, is_string
+from baon.core.plan.__tests__.MakeRenamePlanTestCaseBase import MakeRenamePlanTestCaseBase
 
 
-class TestMakeRenamePlan(FileSystemTestCase):
+class TestMakeRenamePlan(MakeRenamePlanTestCaseBase):
 
     def test_basic(self):
         self._test_make_rename_plan(
@@ -58,17 +47,6 @@ class TestMakeRenamePlan(FileSystemTestCase):
                 ('MoveFile', 'file2', 'entry/file'),
                 ('MoveFile', 'entry_1', 'moved'),
             ))
-
-    def test_file_in_way_will_not_move(self):
-        self._test_make_rename_plan(
-            (
-                ('FILE', 'file1'),
-                ('FILE', 'file2'),
-                ('FILE', 'entry'),
-            ),
-            '"file2"->"entry/file"',
-            ('CannotCreateDestinationDirFileInTheWayWillNotMoveError', {'destination_dir': 'entry'}),
-            filter_scanned_files=lambda file_ref: file_ref.filename != 'entry')
 
     def test_file_in_way_will_move_find_free_name(self):
         self._test_make_rename_plan(
@@ -143,61 +121,6 @@ class TestMakeRenamePlan(FileSystemTestCase):
                 ('MoveFile', 'file4', 'file5'),
                 ('MoveFile', 'file3_1', 'file4')
             ))
-
-    def test_fail_if_renamed_files_have_errors(self):
-        self._test_make_rename_plan(
-            (
-                ('FILE', 'file1'),
-                ('FILE', 'file2'),
-            ),
-            '"file2"->"file1"',
-            ('RenamedFilesListHasErrorsError',))
-
-    def test_ok_if_renamed_files_have_warnings(self):
-        self._test_make_rename_plan(
-            (
-                ('FILE', 'file1'),
-                ('FILE', 'file2'),
-            ),
-            '"file2"->"file  2"',
-            (
-                ('MoveFile', 'file2', 'file  2'),
-            ))
-
-    def _test_make_rename_plan(self, files_repr, rules_text, expected_result, filter_scanned_files=None):
-        base_path = self.resolve_test_path('')
-
-        if len(expected_result) > 0 and is_arrayish(expected_result[0]):
-            expected_result = _apply_base_path_to_expected_result(expected_result, base_path)
-
-        with self.temp_file_structure('', files_repr):
-            files = scan_files(base_path, recursive=True)
-            if filter_scanned_files is not None:
-                files = [file_ref for file_ref in files if filter_scanned_files(file_ref)]
-
-            rule_set = parse_rules(rules_text)
-            renamed_files = rename_files(files, rule_set, use_path=False, use_extension=False)
-
-            try:
-                plan = make_rename_plan(base_path, renamed_files)
-                result = plan.test_repr()
-            except MakeRenamePlanError as e:
-                result = e.test_repr()
-
-            self.assertEquals(
-                result,
-                expected_result,
-            )
-
-
-def _apply_base_path_to_expected_result(expected_result, base_path):
-    return tuple(_apply_base_path_to_action_repr(action_repr, base_path) for action_repr in expected_result)
-
-
-def _apply_base_path_to_action_repr(action_repr, base_path):
-    return (action_repr[0],) + tuple(
-        os.path.join(base_path, value) if is_string(value) else value for value in action_repr[1:]
-    )
 
 
 def _make_permutation_rules(permutation):
---
+++ src/baon/core/plan/__tests__/test_make_rename_plan_errors.py
@@ -0,0 +1,44 @@
+# baon/core/plan/__tests__/test_make_rename_plan_errors.py
+#
+# (C) Copyright 2012-present  Cristian Dinu <goc9000@gmail.com>
+#
+# This file is part of BAON.
+#
+# Licensed under the GPL-3
+
+
+from baon.core.plan.__tests__.MakeRenamePlanTestCaseBase import MakeRenamePlanTestCaseBase
+
+
+class TestMakeRenamePlanErrors(MakeRenamePlanTestCaseBase):
+
+    def test_file_in_way_will_not_move(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'file1'),
+                ('FILE', 'file2'),
+                ('FILE', 'entry'),
+            ),
+            '"file2"->"entry/file"',
+            ('CannotCreateDestinationDirFileInTheWayWillNotMoveError', {'destination_dir': 'entry'}),
+            filter_scanned_files=lambda file_ref: file_ref.filename != 'entry')
+
+    def test_fail_if_renamed_files_have_errors(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'file1'),
+                ('FILE', 'file2'),
+            ),
+            '"file2"->"file1"',
+            ('RenamedFilesListHasErrorsError',))
+
+    def test_ok_if_renamed_files_have_warnings(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'file1'),
+                ('FILE', 'file2'),
+            ),
+            '"file2"->"file  2"',
+            (
+                ('MoveFile', 'file2', 'file  2'),
+            ))
