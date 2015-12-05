335f42e4097b1d45c70e86ef72fb87e5e5022134
Cristian Dinu <goc9000@gmail.com>
2015-12-05 19:25:45 +0000

    New make_rename_plan: Add base path checks

--- src/baon/core/plan/__tests__/test_make_rename_plan_new_errors.py
+++ src/baon/core/plan/__tests__/test_make_rename_plan_new_errors.py
@@ -11,4 +11,49 @@ from baon.core.plan.__tests__.MakeRenamePlanNewTestCaseBase import MakeRenamePla
 
 
 class TestMakeRenamePlanErrors(MakeRenamePlanNewTestCaseBase):
-    pass
+
+    def test_base_path_does_not_exist(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'dummy', 'dummy_renamed'),
+            ),
+            actual_files=(),
+            base_path_override='non_existent',
+            expected_result=('CannotRenameBasePathNotFoundError', {'base_path': 'non_existent'}),
+        )
+
+    def test_base_path_not_a_dir(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'dummy', 'dummy_renamed'),
+            ),
+            actual_files=(
+                ('FILE', 'not_a_dir'),
+            ),
+            base_path_override='not_a_dir',
+            expected_result=('CannotRenameBasePathNotADirError', {'base_path': 'not_a_dir'}),
+        )
+
+    def test_base_path_no_permissions(self):
+        for permission in ['read', 'write', 'execute']:
+            with self.subTest(missing_permission=permission):
+                self._test_make_rename_plan(
+                    (
+                        ('FILE', 'dummy', 'dummy_renamed'),
+                    ),
+                    actual_files=(
+                        ('DIR', 'locked', {permission: False}),
+                        ('FILE', 'locked/dummy'),
+                    ),
+                    base_path_override='locked',
+                    expected_result=('CannotRenameNoPermissionsForBasePathError', {'base_path': 'locked'}),
+                )
+
+    def test_fail_if_renamed_files_have_errors(self):
+        self._test_make_rename_plan(
+            (
+                ('FILE', 'file1'),
+                ('FILE', 'file2', 'file1', ('SyntheticFileError',)),
+            ),
+            ('RenamedFilesListHasErrorsError',),
+        )
--- src/baon/core/plan/make_rename_plan_new.py
+++ src/baon/core/plan/make_rename_plan_new.py
@@ -7,7 +7,15 @@
 # Licensed under the GPL-3
 
 
+import os
+
+from baon.core.files.BAONPath import BAONPath
 from baon.core.plan.RenamePlan import RenamePlan
+from baon.core.plan.__errors__.make_rename_plan_errors import \
+    RenamedFilesListHasErrorsError,\
+    CannotRenameBasePathNotFoundError,\
+    CannotRenameBasePathNotADirError,\
+    CannotRenameNoPermissionsForBasePathError
 
 
 def make_rename_plan(renamed_files):
@@ -16,6 +24,7 @@ def make_rename_plan(renamed_files):
 
 class MakeRenamePlanInstance(object):
     renamed_files = None
+    base_path = None
     steps = []
 
     def __init__(self, renamed_files):
@@ -24,9 +33,28 @@ class MakeRenamePlanInstance(object):
     def run(self):
         self._keep_only_changed_files()
         if len(self.renamed_files) > 0:
-            pass
+            self._check_renamed_files_list()
+            self._compute_base_path()
+            self._check_base_path()
 
         return RenamePlan(self.steps)
 
     def _keep_only_changed_files(self):
         self.renamed_files = [f for f in self.renamed_files if f.is_changed()]
+
+    def _check_renamed_files_list(self):
+        BAONPath.assert_all_compatible(*(renamed_fref.path for renamed_fref in self.renamed_files))
+
+        if any(renamed_fref.has_errors() for renamed_fref in self.renamed_files):
+            raise RenamedFilesListHasErrorsError()
+
+    def _compute_base_path(self):
+        self.base_path = self.renamed_files[0].path.base_path
+
+    def _check_base_path(self):
+        if not os.path.exists(self.base_path):
+            raise CannotRenameBasePathNotFoundError(self.base_path)
+        if not os.path.isdir(self.base_path):
+            raise CannotRenameBasePathNotADirError(self.base_path)
+        if not os.access(self.base_path, os.R_OK | os.W_OK | os.X_OK):
+            raise CannotRenameNoPermissionsForBasePathError(self.base_path)
