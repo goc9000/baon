c09b60c8cc1c47f93dbc2755310f492048ed59d7
Cristian Dinu <goc9000@gmail.com>
2016-01-26 01:27:36 +0000

    Improved UI scanning/selection
    Rearranged the code so that the user can still use the command line parameters to query the known UIs and their problems (previously we would bail immediately if there were no available UIs).

--- src/baon/__main__.py
+++ src/baon/__main__.py
@@ -1,7 +1,7 @@
 # baon/__main__.py
 #
 # (C) Copyright 2012-present  Cristian Dinu <goc9000@gmail.com>
-# 
+#
 # This file is part of BAON.
 #
 # Licensed under the GPL-3
@@ -14,18 +14,5 @@ from baon.ui.ui_info import discover_uis
 
 if __name__ == "__main__":
     ui_info = discover_uis()
-
-    if not any(ui.is_available for ui in ui_info):
-        raise SystemError('No UI is available from among: ' + ', '.join(ui.name for ui in ui_info))
-
     args = parse_app_args(ui_info)
-
-    ui = next(ui for ui in ui_info if ui.name == args.ui)
-
-    if not ui.is_available:
-        raise SystemError("The UI you selected ({0}) is not available for this reason: {1}".format(
-            ui.name,
-            ui.reason_not_available,
-        ))
-
-    ui.start_function(args)
+    args.ui.start_function(args)
--- src/baon/core/app_args.py
+++ src/baon/core/app_args.py
@@ -28,16 +28,14 @@ BAONArguments = namedtuple('BAONArguments', [
 
 
 def parse_app_args(ui_info):
-    assert len(ui_info) > 0
-
-    known_uis = [ui.name for ui in ui_info]
-    uis_help = 'The user interface to use, from among these choices: ' + ', '.join(
-        "'{0}' ({1}{2})".format(
-            ui.name,
-            ui.description,
-            '; NOT AVAILABLE' if not ui.is_available else '',
-        ) for ui in ui_info
-    )
+    if len(ui_info) > 0:
+        uis_help = 'The user interface to use, from among these choices: ' + ', '.join(
+            _format_ui_info(ui) for ui in ui_info
+        )
+        default_ui = next((ui.name for ui in ui_info if ui.is_available), None)
+    else:
+        uis_help = 'The user interface to use (none are installed)'
+        default_ui = None
 
     parser = argparse.ArgumentParser(description=APP_DESCRIPTION)
     parser.add_argument('base_path', metavar='<base path>', nargs='?',
@@ -49,10 +47,24 @@ def parse_app_args(ui_info):
     parser.add_argument('-p', '--use-path', action='store_true', help='Include full path in renaming process')
     parser.add_argument('-o', '--override', action='append', nargs='+', metavar='<from> <to>', dest='overrides',
                         help='Override the rename result for certain files')
-    parser.add_argument('-u', '--ui', choices=known_uis, metavar='<ui>', dest='ui', default=ui_info[0].name,
-                        help=uis_help)
+    parser.add_argument('-u', '--ui', metavar='<ui>', dest='ui', default=default_ui, help=uis_help,
+                        choices=[ui.name for ui in ui_info])
 
     raw_args = parser.parse_args(sys.argv[1:])
+
+    if raw_args.ui is None:
+        if len(ui_info) == 0:
+            parser.error('No UI packages are installed. Install one appropriate for your system and retry.')
+        else:
+            parser.error('No UI is available from among those installed. Address this problem and retry.')
+        sys.exit(-1)
+    else:
+        selected_ui = next(ui for ui in ui_info if ui.name == raw_args.ui)
+        if not selected_ui.is_available:
+            parser.error('The UI you selected ({0}) is not available for this reason: {1}'.format(
+                selected_ui.name,
+                selected_ui.reason_not_available,
+            ))
 
     return BAONArguments(
         base_path=raw_args.base_path,
@@ -61,7 +73,15 @@ def parse_app_args(ui_info):
         use_extension=raw_args.use_extension,
         use_path=raw_args.use_path,
         overrides=_process_overrides_param(parser, raw_args.overrides, raw_args.base_path),
-        ui=raw_args.ui,
+        ui=selected_ui,
+    )
+
+
+def _format_ui_info(ui):
+    return "'{0}' ({1}{2})".format(
+        ui.name,
+        ui.description,
+        '; NOT AVAILABLE: {0}'.format(ui.reason_not_available) if not ui.is_available else '',
     )
 
 
