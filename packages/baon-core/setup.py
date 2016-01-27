414d1d6637e245b2aa967448bf000c88e186affe
Cristian Dinu <goc9000@gmail.com>
2016-01-27 22:09:48 +0000

    Handle baon and baon.ui as namespace packages

--- packages/baon-core/setup.py
+++ packages/baon-core/setup.py
@@ -1,4 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+
+def find_packages_v2(base_dir, namespace_packages=None):
+    result = find_packages(base_dir)
+    for np in (namespace_packages or []):
+        result.append(np)
+        result.extend(np + '.' + p for p in find_packages(os.path.join(base_dir, np.replace('.', os.path.sep))))
+
+    return result
+
 
 setup(
     name='baon-core',
@@ -9,7 +20,7 @@ setup(
     url='https://github.com/goc9000/baon',
     license='GPL-3',
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=find_packages_v2('src', namespace_packages=['baon', 'baon.ui']),
     install_requires=[
         'appdirs',
         'decorator',
