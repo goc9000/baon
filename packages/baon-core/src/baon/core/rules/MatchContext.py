a147311872ea4be56d2ecb5abd2b9d6b559abdf6
Cristian Dinu <goc9000@gmail.com>
2016-03-13 17:17:10 +0000

    Added comment on MatchContext

--- packages/baon-core/src/baon/core/rules/MatchContext.py
+++ packages/baon-core/src/baon/core/rules/MatchContext.py
@@ -10,6 +10,11 @@
 from collections import namedtuple
 
 
+# Note: the reason we keep the entire (text, position) tuple instead of just the incoming text is so that we can
+# correctly handle regexes with lookbehind assertions. This is also the reason why matched_text is a separate value
+# (and we don't just replace the past portion of 'text')
+
+
 MatchContext = namedtuple(
     'MatchContext',
     [
